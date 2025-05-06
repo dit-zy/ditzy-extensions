a5a118791d88d1904ba0232ea112c47112d8c126
ditzy <ditzy.zy@pm.me>
2025-05-06 00:20:02 -0500

    enable vector extensions for netstandard2.0

--- DitzyExtensions/VectorExtensions.cs
+++ DitzyExtensions/VectorExtensions.cs
@@ -1,10 +1,7 @@
 ﻿using System.Numerics;
-#if !NETSTANDARD2_0
 using DitzyExtensions.Generators;
-#endif
 
 namespace DitzyExtensions {
-#if !NETSTANDARD2_0
 	[Swizzle(
 		SourceClass = typeof(Vector2),
 		ComponentsToSwizzle = new[] { nameof(Vector2.X), nameof(Vector2.Y) }
@@ -35,6 +32,5 @@ namespace DitzyExtensions {
 		SourceClass = typeof(Vector4),
 		ComponentsToSwizzle = new[] { nameof(Vector4.X), nameof(Vector4.Y), nameof(Vector4.Z), nameof(Vector4.W) }
 	)]
-#endif
 	public static partial class VectorExtensions { }
 }
