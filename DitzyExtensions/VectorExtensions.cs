5bf88907f71f2d31b7a57ae2c2c759e6f3544f6f
ditzy <ditzy.zy@pm.me>
2025-05-07 12:29:52 -0500

    add additional swizzles for up-sizing vectors

--- DitzyExtensions/VectorExtensions.cs
+++ DitzyExtensions/VectorExtensions.cs
@@ -7,9 +7,31 @@ namespace DitzyExtensions {
 		ComponentsToSwizzle = new[] { nameof(Vector2.X), nameof(Vector2.Y) }
 	)]
 	[Swizzle(
+		SourceClass = typeof(Vector2),
+		TargetClass = typeof(Vector3),
+		NumComponentsToChoose = 3,
+		ComponentsToSwizzle = new[] { nameof(Vector2.X), nameof(Vector2.Y) }
+	)]
+	[Swizzle(
+		SourceClass = typeof(Vector2),
+		TargetClass = typeof(Vector4),
+		NumComponentsToChoose = 4,
+		ComponentsToSwizzle = new[] { nameof(Vector2.X), nameof(Vector2.Y) }
+	)]
+	[Swizzle(
 		SourceClass = typeof(Vector3),
 		TargetClass = typeof(Vector2),
 		NumComponentsToChoose = 2,
+		ComponentsToSwizzle = new[] { nameof(Vector3.X), nameof(Vector3.Y), nameof(Vector3.Z) }
+	)]
+	[Swizzle(
+		SourceClass = typeof(Vector3),
+		ComponentsToSwizzle = new[] { nameof(Vector3.X), nameof(Vector3.Y), nameof(Vector3.Z) }
+	)]
+	[Swizzle(
+		SourceClass = typeof(Vector3),
+		TargetClass = typeof(Vector4),
+		NumComponentsToChoose = 4,
 		ComponentsToSwizzle = new[] { nameof(Vector3.X), nameof(Vector3.Y), nameof(Vector3.Z) }
 	)]
 	[Swizzle(
@@ -17,10 +39,6 @@ namespace DitzyExtensions {
 		TargetClass = typeof(Vector2),
 		NumComponentsToChoose = 2,
 		ComponentsToSwizzle = new[] { nameof(Vector4.X), nameof(Vector4.Y), nameof(Vector4.Z), nameof(Vector4.W) }
-	)]
-	[Swizzle(
-		SourceClass = typeof(Vector3),
-		ComponentsToSwizzle = new[] { nameof(Vector3.X), nameof(Vector3.Y), nameof(Vector3.Z) }
 	)]
 	[Swizzle(
 		SourceClass = typeof(Vector4),
