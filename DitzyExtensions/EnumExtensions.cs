2961fb2e9f4511804aa903dc5eb78aa2bc3dd49d
ditzy <ditzy.zy@pm.me>
2025-05-12 20:51:59 -0500

    add additional AsEnum() functions

--- DitzyExtensions/EnumExtensions.cs
+++ DitzyExtensions/EnumExtensions.cs
@@ -10,9 +10,20 @@ namespace DitzyExtensions {
 			Enum.GetValues(typeof(T)) as T[] ?? Array.Empty<T>();
 #endif
 
-		public static Result<T, string> AsEnum<T>(this string enumName) where T : struct =>
-			Enum.TryParse(enumName, true, out T result)
+		public static T AsEnum<T>(this string enumName) where T : struct =>
+			TryAsEnum<T>(enumName).TryGetValue(out T result)
+				? result
+				: throw new ArgumentException($"Enum '{enumName}' does not exist.");
+
+		public static Result<T, string> TryAsEnum<T>(this string enumName) where T : struct =>
+			enumName.TryAsEnum(out T result)
 				? Result.Success<T, string>(result)
-				: $"";
+				: $"Enum '{enumName}' not found for type '{typeof(T).Name}'.";
+
+		public static bool TryAsEnum<T>(this string enumName, out T result) where T : struct {
+			var foundEnum = Enum.TryParse(enumName, true, out T parseResult);
+			result = parseResult;
+			return foundEnum;
+		}
 	}
 }
