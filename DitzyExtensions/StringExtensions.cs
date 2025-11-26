afa214d5c4758457b42da4b9a3292901dfd0064a
ditzy <ditzy.zy@pm.me>
2025-11-26 05:29:39 -0600

    add string Indent()

--- DitzyExtensions/StringExtensions.cs
+++ DitzyExtensions/StringExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using DitzyExtensions.Functional;
 
 namespace DitzyExtensions {
 	public static class StringExtensions {
@@ -14,12 +16,32 @@ namespace DitzyExtensions {
 		public static string Join(this IEnumerable<string> source, string separator) =>
 #endif
 			string.Join(separator, source);
-		
+
 #if NET6_0_OR_GREATER
 		public static string Join(this IEnumerable<char> source, string? separator) =>
 #else
 		public static string Join(this IEnumerable<char> source, string separator) =>
 #endif
 			string.Join(separator, source);
+
+		public static string Indent(this string str, int level, int indentSize = 4, string indentChar = " ") =>
+			indentChar
+				.Repeat(indentSize)
+				.Join(null)
+				.Repeat(level)
+				.Join(null)
+			+ str;
+
+		public static string IndentLines(
+			this string str,
+			int level,
+			int indentSize = 4,
+			string indentChar = " ",
+			string newlineChar = "\n"
+		) =>
+			str
+				.Split(newlineChar.ToCharArray())
+				.Select(line => line.Indent(level, indentSize, indentChar))
+				.Join(newlineChar);
 	}
 }
