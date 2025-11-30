51b56e7ee46dbbd7a8d1159c9f2d4e49cbe4f86c
ditzy <ditzy.zy@pm.me>
2025-11-30 04:04:20 -0600

    add the first exception extensions

---
+++ DitzyExtensions/ExceptionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CSharpFunctionalExtensions;
+
+namespace DitzyExtensions {
+	public class ExceptionExtensions {
+		public static Result<T, U> Try<T, U, E>(Func<T> action, Func<E, U> catchAction) where E : Exception {
+			try {
+				return action();
+			} catch (E e) {
+				return catchAction(e);
+			}
+		}
+	}
+}
